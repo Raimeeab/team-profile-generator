244807408ca34aca9adfbdf602a8eab62fc90f88
Raimeeab <raimee.abbassi@gmail.com>
2021-10-27 18:19:46 +1100

    Debug employee.test.js

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -1,4 +1,27 @@
+// Testing employee constructor
+const { TestWatcher } = require("jest");
+const Employee = require("../lib/employee");
 const employee = require("../lib/employee");
 
+describe("Employee", () => {
+    it("Should create Employee constructor", () => {
+        const employee = new Employee("Raimee Abbassi", 707, "raimeeab@gmail.com");
 
+        expect(typeof(employee).toEqual("object"))
+    });
 
+    it("Should create name in employee constructor param", () => {
+        const name = "Raimee Abbassi";
+        const employeeName = new employee(name);
+        
+        expect(employee.name).toEqual(name);
+    });
+
+    it("Should create employee ID in constructor param", () => {
+        const testId = 707;
+        const employeeId = new employee("Raimee Abbassi", testId);
+    
+        expect(typeof(employee.id).toBe("Number"));
+        expect(employee.id).toEqual(id);
+    });
+});
--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 // node modules
 const inquirer = require("inquirer");
 const fs = require("fs");
+const path = require("path");
 
 // Link to create page 
 const generatePage = require("./src/generateHTML");
 
-// team profiles
+// Team profiles
 const employee = require("./lib/employee");
 const engineer = require("./lib/engineer");
 const intern = require("./lib/intern");
 const manager = require("./lib/manager");
+
+// Empty array for team members
+const teamMembers = [];
+
+
+const userPrompts = [
+    {
+        type: "input",
+        name: "manager",
+        message: "Manager of team:",
+        // validate: ""
+    },
+    {
+
+    }
+]
